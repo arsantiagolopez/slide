613abaecaa23b1f4c77554faab03d2f7817dc098
Alex Santiago Lopez <alexsantiagolopez@Alexs-MacBook-Pro.local>
2021-07-10 16:34:41 -0500

    More Heroku deployment config.

--- server/src/config.js
+++ server/src/config.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 export default {
   // Development
   nodeEnv: process.env.NODE_ENV || "production",
-  port: process.env.PORT || 4000,
+  port: process.env.PORT || 8000,
   cookieName: process.env.COOKIE_NAME || "cid",
   sessionSecret: process.env.SESSION_SECRET || "Auth Secret Key",
   databaseUrl: process.env.DATABASE_URL,
